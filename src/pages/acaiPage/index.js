1c96f939f86e6efce5527bc2569daa8315f85da7
luan r <anjobranco67@gmail.com>
2022-10-07 08:12:07 -0300

    add funcao calcularAcai

--- src/pages/acaiPage/index.js
+++ src/pages/acaiPage/index.js
@@ -1,7 +1,12 @@
 
 
 export default function AcaiPage () {
-
+    
+function calcularAçai (qtdPeq, qtdMed, qtdGra, desc ) {
+    let soma = (qtdPeq * 13.50 + qtdMed * 15.00 + qtdGra * 17.50) 
+    let totalcomDesc = (soma / 100) * desc
+    return soma - totalcomDesc
+}
     return (
         <main>
             
