4e86a6f715c9a8766a10ba66b6bdf7c8e8ec0f09
luan r <anjobranco67@gmail.com>
2022-10-07 08:09:07 -0300

    Layout + RouterDom

---
+++ src/pages/acaiPage/index.js
@@ -0,0 +1,10 @@
+
+
+export default function AcaiPage () {
+
+    return (
+        <main>
+            
+        </main>
+    )
+}
---
+++ src/pages/libraPage/index.js
@@ -0,0 +1,10 @@
+
+
+export default function AcaiPage () {
+
+    return (
+        <main>
+            <h1>Libra</h1>
+        </main>
+    )
+}
