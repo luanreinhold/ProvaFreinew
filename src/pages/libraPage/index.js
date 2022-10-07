a304d17c0eec74d0db71d91211559a71ed413201
luan r <anjobranco67@gmail.com>
2022-10-07 09:43:24 -0300

    add funcao da libra

--- src/pages/libraPage/index.js
+++ src/pages/libraPage/index.js
@@ -1,12 +1,37 @@
+import { useState } from "react"
+import CompHeader from "../../components/Header"
 import "./index.scss"
 
-export default function libraPage () {
+export default function LibraPage () {
+    
+    const [mes, setMes] = useState("")
+    const [dia, setDia] = useState(0)
+    const [resposta, setResposta] = useState("")
+
+    function verificarLibra () {
+    
+        if(mes === "Setembro" && dia > 23 && dia < 30 || mes === "Outubro" && dia < 22 ) {
+            return setResposta("É libra")
+        } else {
+            return setResposta("Não é libra")
+        }
+    }
 
     return (
+        
         <main className="libraPage">
-            <h1>Libra</h1>
-
-
+            
+            <CompHeader/>
+          
+                <section className="boxLibra">
+                    <h1>Verificar Signo</h1>
+                    <input placeholder="mes" value={mes} onChange={e => setMes(e.target.value)}/>
+                    <input placeholder="dia" value={dia} onChange={e => setDia(Number(e.target.value))}/>
+                    <button onClick={verificarLibra}>Verificar</button>
+                    {resposta}
+                </section>
+        
         </main>
+        
     )
 }
